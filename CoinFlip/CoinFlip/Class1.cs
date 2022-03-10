1ab7ea0cce3716cc14c91be4105c5222644339bb
sanjay <ssah6694@gmail.com>
2022-03-11 00:21:02 +0530

    [sanjay]add: added flip coin

---
+++ CoinFlip/CoinFlip/Class1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoinFlip
+{
+    public class Class1
+    {
+        public static void flip(int tossnumber)
+        {
+            int Head = 0;
+            int Tail = 1;
+            Random obj = new Random();
+            while (tossnumber > 1)
+            {
+                int randomCheck = obj.Next(0, 2);
+                if (randomCheck == 0)
+                {
+                    Head++;
+                }
+                else
+                {
+                    Tail++;
+                }
+                tossnumber--;
+            }
+            Console.WriteLine("Head= " + Head);
+            Console.WriteLine("Tail= " + Tail);
+        }
+    }
+}
---
+++ CoinFlip/CoinFlip/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace CoinFlip
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("please enter number you want to flip");
+            int tossnumber = int.Parse(Console.ReadLine());
+            Class1.flip(tossnumber);
+        }
+    }
+}
